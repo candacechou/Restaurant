28e3a3a6931a57705f8c02eadfc4c4626ebb2cd5
candacechou <candace01250125@gmail.com>
2023-08-14 11:56:40 +0800

    add seeder file from json file

---
+++ seeders/20230814033558-initial-data.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const sample = require('../public/jsons/restaurant.json');
+
+    await queryInterface.bulkInsert('restaurant_info', sample.results, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('restaurant_info', null, {});
+  }
+
+}
+
