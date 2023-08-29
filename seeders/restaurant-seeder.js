75a7dc8ec5754530bbeea3f7f0430ef0fdc3845c
candacechou <candace01250125@gmail.com>
2023-08-30 01:10:09 +0800

    fix: npm run seed with primary key problem on user

--- seeders/restaurant-seeder.js
+++ seeders/restaurant-seeder.js
@@ -1,63 +1,46 @@
 'use strict';
-const bcrypt = require('bcryptjs')
-/** @type {import('sequelize-cli').Migration} */
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-
     const sample = require('../public/jsons/restaurant.json');
-    const user = require('../public/jsons/user.json')
-    let transaction
-    let users = {};
-    await Promise.all(user.results.map(async (person, index, array) => {
+    const user = require('../public/jsons/user.json');
+    let users = {}
+    // Hash passwords using bcrypt
+    await Promise.all(user.results.map(async (person) => {
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(person.password, salt);
       person.password = hash;
-      users[person.id] = person.collection;
-      delete array[index]['collection']
+      person.collection.forEach((ids) => { users[ids] = person.id; })
+      delete person.collection;
     }));
-    transaction = await queryInterface.sequelize.transaction();
+
     try {
-      await queryInterface.bulkInsert('Users', user.results, { transaction });
-      await Promise.all(sample.results.map(async (restaurant, index, array) => {
-        let new_id = 0
-        for (let i = 0; i < Object.keys(users).length; i++) {
-          let key = Object.keys(users)[i]
-          if (users[key].indexOf(array[index].id) !== -1) {
-            new_id = parseInt(key)
-          }
-        }
-        array[index].userId = new_id
-      }));
-      // insert restaurant
-      await queryInterface.bulkInsert('restaurants', sample.results, { transaction });
-      // Commit the transaction
-      await transaction.commit();
+      console.log(user.results)
+      // Insert users
+      await queryInterface.bulkInsert('Users', user.results, {});
+      console.log("done user")
     } catch (error) {
-      await transaction.rollback();
-      console.log(error)
+      console.error("Error inserting users:", error);
     }
+
+    // Associate users with restaurants and insert restaurants
+    sample.results.forEach((restaurant, index, array) => {
+      if (users.hasOwnProperty(restaurant.id)) {
+        array[index].userId = users[restaurant.id]
+      }
+      else {
+        array[index].userId = 0
+      }
+    });
+    console.log(sample.results)
+
+    await queryInterface.bulkInsert('Restaurants', sample.results, {});
 
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-    await queryInterface.bulkDelete('restaurants', null, {});
+    await queryInterface.bulkDelete('Restaurants', null, {});
     await queryInterface.bulkDelete('Users', null, {});
   }
-
-}
-
+};
